60228b6b828832a2c9172fbfb7fd5ab18fb75437
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-01-20 20:17:05 +0900

    Update 9n_PLUS_1.py

--- 9n_PLUS_1.py
+++ 9n_PLUS_1.py
@@ -77,5 +77,5 @@ with open('HailstoneArray.json', 'w') as file:
 if (max(Hailstone_Num) > int(maximum["maximum"])):
   maximum["n_sequence"] = n_sequence + new_Hailstone_Num_array.index(max(Hailstone_Num)) + 1
   maximum["maximum"] = max(Hailstone_Num)
-  with open('maximum.txt', 'w') as file:
+  with open('maximum.json', 'w') as file:
     file.write(str(maximum))
